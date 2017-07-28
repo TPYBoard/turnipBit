5d213b778362ca46b9fbc06d15d357603c72425a
TPYBoard <info@turnipsmart.com>
2017-07-28 15:51:38 +0800

    conf

--- conf.py
+++ conf.py
@@ -190,7 +190,7 @@ html_additional_pages = {"index": "topindex.html"}
 #html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
